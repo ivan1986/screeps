6be8549e890efdde9d8cfea09e3f67e715c271a8
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2020-06-15 12:45:36 +0300

    deploy

--- harvesterLDSettings.js
+++ harvesterLDSettings.js
@@ -2,5 +2,10 @@
  * перечисление комнат room (по имени), в которых может максимум быть по limit крипов-харвейстеров
  */
 //Creep.prototype.resourseRooms.push({room: 'W4N3', limit: 3});
-Creep.prototype.resourseRooms.push({room: 'W6N3', limit: 3});
-Creep.prototype.resourseRooms.push({room: 'W5N2', limit: 2});
+//Creep.prototype.resourseRooms.push({room: 'W6N3', limit: 3});
+//Creep.prototype.resourseRooms.push({room: 'W5N2', limit: 2});
+
+Creep.prototype.resourseRooms.push({room: 'W12S22', limit: 2});
+Creep.prototype.resourseRooms.push({room: 'W14S22', limit: 2});
+Creep.prototype.resourseRooms.push({room: 'W13S23', limit: 3});
+
